87c70a553a1dd374a4eeadad801588793edc310b
sawthompson <Sawyer@Sawyers-MacBook-Pro.local>
2013-11-12 13:36:17 -0500

    XML sound data support and a mainline that loads it

--- src/engine/sound/SoundHolder.java
+++ src/engine/sound/SoundHolder.java
@@ -1,6 +1,13 @@
 package engine.sound;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Hashtable;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 
 /**
  * A setup to make sound management a little bit easier.
@@ -13,13 +20,39 @@ public class SoundHolder {
 
 	public static final Hashtable<String, Sound> sounds = new Hashtable<String, Sound>();
 	
-	/**
-	 * Creates a new sound from the given path and adds it to the Hashtable with the given name as a key
-	 * 
-	 * @param name
-	 * @param path
-	 */
-	public void addSound(String name, String path) {
-		sounds.put(name, new Sound(path));
+	public SoundHolder(String toRead) {
+		String tagContent = null;
+		XMLInputFactory factory = XMLInputFactory.newInstance();
+		try {
+			XMLStreamReader reader = factory.createXMLStreamReader(toRead, new FileInputStream(toRead));
+			String currSoundID = null;
+			while(reader.hasNext()) {
+				int event = reader.next();				
+				switch(event) {
+				case XMLStreamConstants.START_ELEMENT:
+					if("sound".equals(reader.getLocalName())) {
+						currSoundID = reader.getAttributeValue(0);
+					}
+					break;
+				
+				case XMLStreamConstants.CHARACTERS:
+					tagContent = reader.getText().trim();
+					break;
+				
+				case XMLStreamConstants.END_ELEMENT:
+					String name = reader.getLocalName();
+					if(name == "file") {
+						sounds.put(currSoundID, new Sound(tagContent));
+					}
+				}
+			}
+		
+		} catch (XMLStreamException e) {
+			System.out.println("Could not read XML");
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			System.out.println("Could not locate file");
+			e.printStackTrace();
+		}
 	}
 }
---
+++ src/game/gameMain.java
@@ -0,0 +1,10 @@
+package game;
+
+import engine.sound.SoundHolder;
+
+public class gameMain {
+
+	public static void main(String[] args) {
+		SoundHolder s = new SoundHolder("testSounds.xml");
+	}
+}
